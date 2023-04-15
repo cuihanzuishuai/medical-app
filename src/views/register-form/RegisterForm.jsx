577ad8e3524fecb27747473b8523eebd5029f50a
cole <wangyiqing@up366.com>
2023-04-15 21:45:30 +0800

    feat: 报表登记

--- src/views/register-form/RegisterForm.jsx
+++ src/views/register-form/RegisterForm.jsx
@@ -1,4 +1,8 @@
-import { defineComponent } from 'vue'
+import { defineComponent, reactive, ref, onMounted, nextTick } from 'vue'
+import { Field, Icon, NavBar, SwipeCell, Space, Empty, showNotify } from 'vant'
+import Loading from '@/components/loading'
+import { useRouter } from 'vue-router'
+import { requestStatisticsCustomer } from '@/api/statistics'
 import classNames from '@/common/classNamesBind'
 import styles from './style/index.module.scss'
 
@@ -6,9 +10,44 @@ const cx = classNames.bind(styles)
 
 export default defineComponent({
     setup () {
+        const router = useRouter()
+
+        const formData = reactive({
+            mobile: '', // 员工手机号
+            name: '' // 员工姓名
+        })
+
+        function onBackPrev () {
+            router.go(-1)
+        }
+
         return () => {
             return (
-                <div>RegisterForm</div>
+                <div class={ cx('view-wrap', 'wrap-flex') }>
+                    <NavBar
+                        title="报表登记"
+                        leftArrow={ true }
+                        onClickLeft={ onBackPrev }
+                    />
+                    <div class={ cx('scroll-wrap') }>
+                        <Field
+                            label="客户姓名"
+                            placeholder="请输入"
+                            v-model={ formData.name }
+                        />
+                        <Field
+                            label="客户电话"
+                            placeholder="请输入"
+                            v-model={ formData.mobile }
+                        />
+                        <Field
+                            label="预期到访时间"
+                            placeholder="请选择"
+                            readonly={ true }
+                            isLink={ true }
+                        />
+                    </div>
+                </div>
             )
         }
     }
